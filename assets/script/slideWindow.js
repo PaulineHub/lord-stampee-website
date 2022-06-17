eedb88d6fa4c71904f79de3d26db0fdd1127a757
Pauline <huby.pauline@gmail.com>
2022-06-16 21:38:50 -0400

    ajout navigation, debut aside et grid catalogue

---
+++ assets/script/slideWindow.js
@@ -0,0 +1,10 @@
+/* Open when someone clicks on the span element */
+function openSideWindow() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeSideWindow() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
