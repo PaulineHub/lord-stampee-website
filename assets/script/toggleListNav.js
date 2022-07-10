bdd0288c7491cf64ea46b07ebf466a4913c655be
Pauline <huby.pauline@gmail.com>
2022-07-10 18:33:18 -0400

    suppression chevron nav mobile

--- assets/script/toggleListNav.js
+++ assets/script/toggleListNav.js
@@ -3,19 +3,23 @@
 const toggleBtnsNav = document.querySelectorAll('.dropbtn');
 
 toggleBtnsNav.forEach(toggleBtn => {
+
     toggleBtn.addEventListener('click', () => {
+
         let divToToggle = toggleBtn.nextElementSibling;
         let retractBtnNav = toggleBtn.firstElementChild.nextElementSibling;
         let developBtnNav = retractBtnNav.nextElementSibling;
+
         if(divToToggle.classList.contains('list-hidden')) {
             divToToggle.classList.remove('list-hidden');
-            developBtnNav.style.display = 'none';
-            retractBtnNav.style.display = 'contents';
+            developBtnNav.classList.add('hide-chevron-btn');
+            retractBtnNav.classList.add('show-chevron-btn');
         } else {
             divToToggle.classList.add('list-hidden');
-            retractBtnNav.style.display = 'none';
-            developBtnNav.style.display = 'contents';
+            retractBtnNav.classList.remove('show-chevron-btn');
+            developBtnNav.classList.remove('hide-chevron-btn');
         }
+
     })
 })
 
